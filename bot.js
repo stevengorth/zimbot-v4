32635de3b40ad552c00c887428a73c137ca668ce
stevengorth <117027004+stevengorth@users.noreply.github.com>
2022-10-30 21:17:00 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,10 +33,10 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['14123858486',''] //owner number +14123858486
+global.wame = ['14123858486'] 
+global.pemilik = ['14123858486'] //owner number +14123858486
+global.premium = ['14123858486'] //premium number +14123858486
 global.pengguna = 'Drips' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
